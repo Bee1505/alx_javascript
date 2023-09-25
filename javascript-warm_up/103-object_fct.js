4e5caf1f5c8699db4a73a10dbc6a2f8a08d3a5c8
beatricewairimu723@gmail.com <beatricewairimu723@gmail.com>
2023-09-25 18:27:39 +0300

    New Commit

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -2,11 +2,17 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function() {
+  incr: function () {
     this.value++;
   }
 };
+
 console.log(myObject);
+
+Object.defineProperty(myObject, 'incr', {
+  enumerable: false, // Prevent the function from being enumerated
+  value: myObject.incr
+});
 
 myObject.incr();
 console.log(myObject);
