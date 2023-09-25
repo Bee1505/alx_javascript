acbd6ade80bb1df21435512f454fd8adaa713570
beatricewairimu723@gmail.com <beatricewairimu723@gmail.com>
2023-09-25 18:32:49 +0300

    New Commit

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-const add = require('./13-add').add;
 
-const result1 = add(4, 85);
-console.log(`Correct output - 4 + 85: ${result1}`);
+function add(a, b) {
+  return a + b;
+}
 
-const result2 = add(93, -4);
-console.log(`Correct output - 93 + -4: ${result2}`);
-
-const result3 = add(0, 89);
-console.log(`Correct output - 0 + 89: ${result3}`);
-
+module.exports.add = add;
 
 
